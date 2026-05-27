7b694b38172edd65e5b760418b0cccb0251c31b9
Abdelrahman Hamed <body.hamed@outlook.com>
2026-05-27 14:22:13 +0300

    Updated OrderForm.tsx

--- src/components/OrderForm.tsx
+++ src/components/OrderForm.tsx
@@ -72,7 +72,7 @@ export default function OrderForm({ onSubmit, isSubmitting }: OrderFormProps) {
 
       const finalFormData: OrderFormData = {
         ...formData,
-        AuraQuantity: AuraQty,
+        auraQuantity: auraQty,
         HarmoniaQuantity: HarmoniaQty,
         SophiaQuantity: SophiaQty,
       };
