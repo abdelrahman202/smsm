55a92d18e70291f64dd27bfff5fdddeb5df39c91
Abdelrahman Hamed <body.hamed@outlook.com>
2026-05-27 13:23:23 +0300

    Updated OrderForm.tsx

--- src/components/OrderForm.tsx
+++ src/components/OrderForm.tsx
@@ -16,7 +16,7 @@ export default function OrderForm({ onSubmit, isSubmitting }: OrderFormProps) {
     address: '',
     AuraQuantity: 0,
     HarmoniaQuantity: 0,
-    curvedGoldQuantity: 0,
+    SophiaQuantity: 0,
   });
 
   const [errors, setErrors] = useState<Partial<OrderFormData>>({});
