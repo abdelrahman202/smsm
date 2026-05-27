7f1b6ac397806701fff89e020a8ea87e6fc4c113
Abdelrahman Hamed <body.hamed@outlook.com>
2026-05-27 14:26:30 +0300

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -52,7 +52,7 @@ function App() {
     setIsSubmitting(true);
 
     try {
-      const totalQuantity = data.auraQuantity + data.HarmoniaQuantity + data.SophiaQuantity;
+      const totalQuantity = data.auraQuantity + data.harmoniaQuantity + data.SophiaQuantity;
       const totalPrice = totalQuantity * 460;
 
       const formDataToSend = {
